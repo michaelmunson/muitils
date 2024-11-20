7349c48f9bafc580023a01739707793940fb9316
Michael Munson <michael.s.munson@outlook.com>
2024-11-19 22:34:55 -0500

    deploy minor

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ export * from './sx';
 export * from './Button'
 export {default as Button} from './Button';
 
-export * from './Flex'
+export {Col, Row, type FlexProps, type ColProps, type RowProps} from './Flex'
 export {default as Flex} from './Flex';
 
-export * from './Icon'
 export {default as Icon} from './Icon';
 
-export * from './Input'
-
-export * from './Spinner'
 export {default as Spinner} from './Spinner';
 
 export {default as Form} from './Form';
+
+export * from './Input'
