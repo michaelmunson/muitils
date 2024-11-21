5ea7420fb265e80be899ecdec5c6f27f82e58439
Michael Munson <michael.s.munson@outlook.com>
2024-11-21 07:58:51 -0500

    3.3.1 - error in Table.utils

--- src/Table/utils.ts
+++ src/Table/utils.ts
@@ -5,17 +5,18 @@ type HeadArgs = [props:MuiTableHeadProps, cells:TableCellInput[]] | [cells:Table
 type BodyArgs = [props:MuiTableBodyProps, cells:TableRowInput[]] | [cells:TableRowInput[]]
 type RowArgs = [props:MuiTableRowProps, cells:TableCellInput[]] | [cells:TableCellInput[]]
 
-export const body = (args:BodyArgs) => ({
+export const body = (...args:BodyArgs) => ({
   ...(Array.isArray(args) ? args[0] : {}),
   rows: Array.isArray(args) ? args[1] : args
 });
 
-export const row = (args:RowArgs) => ({
+export const row = (...args:RowArgs) => ({
   ...(Array.isArray(args) ? args[0] : {}),
   cells: Array.isArray(args) ? args[1] : args
 })
 
-export const head = (args:HeadArgs) => ({
+export const head = (...args:HeadArgs) => ({
   ...(Array.isArray(args) ? args[0] : {}),
   cells: Array.isArray(args) ? args[1] : args
 });
+
